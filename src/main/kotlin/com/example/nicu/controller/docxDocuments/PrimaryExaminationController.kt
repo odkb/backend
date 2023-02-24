027ef3d025bd6b101bd9f925627dc83629b14c64
Danil <74546538+Gladioluss@users.noreply.github.com>
2023-02-24 13:11:54 +0500

    Update controller to receive data on the initial inspection

--- src/main/kotlin/com/example/nicu/controller/docxDocuments/PrimaryExaminationController.kt
+++ src/main/kotlin/com/example/nicu/controller/docxDocuments/PrimaryExaminationController.kt
@@ -1,6 +1,9 @@
 package com.example.nicu.controller.docxDocuments
 
+import com.example.nicu.dto.docxDocumentsDto.PrimaryExaminationDto
 import mu.KotlinLogging
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,4 +12,12 @@ private val logger = KotlinLogging.logger {}
 @RestController
 @RequestMapping("/api/docxDocuments/primaryExamination")
 class PrimaryExaminationController {
+
+    @GetMapping("/getData")
+    fun getData(@RequestBody body: PrimaryExaminationDto){
+        if(body != null){
+            val json = body
+            println(json)
+        }
+    }
 }
--- src/main/kotlin/com/example/nicu/service/docxDocuments/PrimaryExamination.kt
+++ src/main/kotlin/com/example/nicu/service/docxDocuments/PrimaryExamination.kt
@@ -4,22 +4,42 @@ import org.apache.poi.openxml4j.opc.OPCPackage
 import org.apache.poi.xwpf.usermodel.XWPFDocument
 import java.io.FileOutputStream
 
+val arrayTemplates = arrayListOf<String>("receiptDate", "arrivalTime", "fullNameChild", "comeFurtherTreatmentAndExamination",
+    "born", "admissionAge", "comesFrom", "EPIDNumber", "motherDateBirth", "familyStatus", "maternalIllnesses",
+    "motherBloodGroup", "HIVTestingMother", "HIVTestingFather", "maternalInfectiousHistory", "pregnancy",
+    "childbirth", "previousPregnancies", "dataSiblings", "featuresCoursePregnancy", "steroidProphylaxis",
+    "gestationalAge", "featuresCourseChildbirth", "presentation", "methodDelivery", "durationLabor",
+    "strainingPeriod", "anhydrousPeriod", "waters", "gender", "birthWeight", "length", "headCircumference",
+    "chestCircumference", "apgarScore", "amountAssistanceDeliveryRoom", "diseaseHistory", "respiratorySupport",
+    "drugTherapy", "deeding", "venousAccess", "phototherapy", "hypothermia", "statusAtAdmission", "severityDue",
+    "reactionInspection", "convulsions", "muscleTone", "reflexesNewborn", "bulbarDisorders", "skullShape",
+    "cephalhematomas", "skullSutures", "bigFontanel", "meningealSymptoms", "statusAtAdmissionOther", "skeletonBones",
+    "skullBones", "clavicle", "joints", "skinColor", "damage", "hematomas", "rashes", "pathologicalFormations",
+    "peeling", "edema", "elasticity", "turgor", "cordRemnant", "umbilicalWound", "mucous", "oxygenDependence",
+    "chestShape", "breathingThroughNose", "biomechanicsRespiration", "auscultatoryPicture", "respirationRate",
+    "hemodynamics", "heartSounds", "heartRate", "noise", "pulseDetermined", "paleSpotSymptom", "stomach",
+    "peristalsis", "liver", "spleen", "feeding", "bowelMovement", "kidneys", "diuresis", "stimulationDiuresis",
+    "structureExternalGenitalia", "externalGenitalsFeatures", "mainSyndromesAdmission", "diagnosisAdmission",
+    "surveyPlan", "carePlan", "treatmentPlan", "parenteralNutrition", "nutritionCalculation", "childBloodType",
+    "textConclusion",)
+
 class PrimaryExamination {
     fun fillDocument(path: String){
-        val doc = XWPFDocument(OPCPackage.open(path))
-        for (p in doc.paragraphs) {
-            val runs = p.runs
-            if (runs != null) {
-                for (r in runs) {
-                    var text = r.getText(0)
-                    if (text != null) {
-                        text = text.replace("FIO", "Емурашин Данил Евгеньевич")
-                        text = text.replace("NUMBEREPICRICE", "п.22")
-                        r.setText(text, 0)
+        val doc = XWPFDocument(OPCPackage.open("src/main/resources/templates/primary_examination_OPN.docx"))
+        val paragraphs =  doc.paragraphs
+        for (param in arrayTemplates){
+            for (p in paragraphs) {
+                val runs = p.runs
+                if (runs != null) {
+                    for (r in runs) {
+                        var text = r.getText(0)
+                        if (text != null) {
+                            text = text.replace(param, "0")
+                            r.setText(text, 0)
+                        }
                     }
                 }
             }
         }
-        doc.write(FileOutputStream("output.docx"))
     }
 }
