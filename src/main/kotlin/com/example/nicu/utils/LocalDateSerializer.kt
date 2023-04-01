f451bdd4d0de7a6670969332a1c490083f9175de
DFjay <pezhikov@gmail.com>
2023-04-01 16:43:06 +0500

    Add custom date serializer

---
+++ src/main/kotlin/com/example/nicu/utils/LocalDateSerializer.kt
@@ -0,0 +1,23 @@
+package com.example.nicu.utils
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializer(forClass = LocalDate::class)
+object LocalDateSerializer : KSerializer<LocalDate> {
+    private val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+
+    override fun serialize(encoder: Encoder, value: LocalDate) {
+        encoder.encodeString(value.format(formatter))
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDate {
+        return LocalDate.parse(decoder.decodeString(), formatter)
+    }
+}
