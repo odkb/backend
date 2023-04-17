ae6e554f1f6a6e37a183d68703b1f83813c97c6c
DFjay <pezhikov@gmail.com>
2023-04-17 13:57:19 +0500

    Add more date and time function extension

--- src/main/kotlin/com/example/nicu/utils/DateTimeExtension.kt
+++ src/main/kotlin/com/example/nicu/utils/DateTimeExtension.kt
@@ -1,9 +1,15 @@
 package com.example.nicu.utils
 
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 fun LocalDate.formatToddMMyyyy(): String {
     val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+    return this.format(formatter)
+}
+
+fun LocalDateTime.formatToddMMyyyyHHmm(): String {
+    val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
     return this.format(formatter)
 }
