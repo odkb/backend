d97e8255612cfe3fadca3920ed8af852923f5625
DFjay <pezhikov@gmail.com>
2023-04-17 13:58:27 +0500

    Add handler localDateTime for Documents

--- src/main/kotlin/com/example/nicu/DtoFieldMap.kt
+++ src/main/kotlin/com/example/nicu/DtoFieldMap.kt
@@ -2,7 +2,9 @@ package com.example.nicu
 
 import com.example.nicu.dto.docxDocumentsDto.DocumentDto
 import com.example.nicu.utils.formatToddMMyyyy
+import com.example.nicu.utils.formatToddMMyyyyHHmm
 import java.time.LocalDate
+import java.time.LocalDateTime
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.memberProperties
@@ -28,6 +30,7 @@ class DtoFieldMap<T : DocumentDto>(val dto: T) {
             is List<*> -> field.joinToString(", ")
             null, "" -> EMPTY_FIELD_VALUE
             is LocalDate -> field.formatToddMMyyyy()
+            is LocalDateTime -> field.formatToddMMyyyyHHmm()
             else -> field.toString()
         }
     }
