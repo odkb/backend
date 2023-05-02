1a55b9c8284ac66be4406876926f5ef950ca4832
DFjay <pezhikov@gmail.com>
2023-05-02 21:45:30 +0500

    Use 8.1.1 for Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.6"
     id("io.spring.dependency-management") version "1.1.0"
+    id("org.sonarqube") version "4.0.0.2929"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
